53c37cf615deaba9f2b931bb42e5d09c330b4ccf
Camila <camila@>
2018-11-05 13:36:37 -0200

    teste

---
+++ database/seeds/musicasTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class musicasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
